8edfa3e7cf0a2ac5bf2781a38bbaab268c051583
fbottino2024 <fbottino@campus.ungs.edu.ar>
2024-06-02 11:52:12 -0300

    ejercicio 7 resuelto

---
+++ practica-2/epromedio.py
@@ -0,0 +1,8 @@
+numero1=float(input("Ingrese un número"))
+numero2=float(input("Ingrese un número"))
+promedio=(numero1+numero2)/2
+print(promedio)
+if promedio >7:
+    print("Aprobado")
+else:    
+    print("Desaprobado")
